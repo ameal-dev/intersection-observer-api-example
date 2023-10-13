cc855dea5da020c6c75ea07e76af7c74a0393700
Emil Rydén <emil.ryden2@ingka.com>
2023-10-13 10:43:46 +0200

    intersection observer API example

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,76 @@
+'use client'
+import { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
-import styles from './page.module.css'
+import gifImage from '../../public/rick_astley.gif'
 
-export default function Home() {
+function App() {
+  const boxRef = useRef(null)
+  const [intersectionPercentage, setIntersectionPercentage] = useState(0)
+
+  const inFullView = intersectionPercentage === 100
+
+  useEffect(() => {
+    const options = {
+      root: null, // Use the viewport as the root
+      rootMargin: '0px',
+      // To achieve a resolution of 1%
+      // we create an array [0,0.01,0.02...1.0]
+      threshold: Array.from({ length: 101 }, (_, i) => i / 100),
+    }
+
+    // IntersectionCallback is a function that gets called by the Intersection Observer
+    // for each target element when its intersection with the viewport changes.
+    const interSectionCallback = (entries) => {
+      console.log(entries)
+      entries.forEach((entry) => {
+        console.log(entry)
+        // Calculate the percentage of the target element that is currently visible
+        // in the viewport.
+        const percentage = Math.floor(entry.intersectionRatio * 100)
+        // Update the component's state with the calculated percentage.
+        setIntersectionPercentage(percentage)
+      })
+    }
+
+    const observer = new IntersectionObserver(interSectionCallback, options)
+
+    // Check if the box-element exists in the DOM
+    if (boxRef.current) {
+      observer.observe(boxRef.current)
+    }
+
+    // Clean up the observer when the component unmounts
+    return () => observer.disconnect()
+  }, [])
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div
+      className='App'
+      style={{
+        height: '150vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'end',
+      }}
+    >
+      <div
+        className='box'
+        ref={boxRef}
+        style={{
+          width: '320px',
+          height: '240px',
+          backgroundColor: inFullView ? 'blue' : 'orange',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'start',
+          fontSize: '24px',
+          margin: '0 0 50px 0',
+        }}
+      >
+        <span>{intersectionPercentage}% Visible</span>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
+
+export default App
